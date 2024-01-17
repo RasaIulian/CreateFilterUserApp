c5040c4e8c2f43d1ede91fee2aebd51cb74b4cef
RasaIulian <ilie.rasa@gmail.com>
2024-01-17 16:51:58 +0200

    styling and improved functionality

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 // - {id: 1, name: ‘User 1’}
 // - {id: 2, name: ‘User 2’}
 const usersList = document.getElementById("usersList");
+const searchIcon = document.getElementById("searchIcon");
 
 generateUsers = () => {
   let users = [];
   for (let i = 1; i <= 100; i++) {
     users.push({ id: i, name: "User " + i });
   }
-  console.log("Users:", users);
+  // console.log("Users:", users);
   return users;
 };
 
@@ -17,6 +18,7 @@ displayUsers = () => {
   usersList.innerHTML = "";
   document.getElementById("search").value = "";
   let users = generateUsers();
+  searchIcon.style.display = "inline-block";
 
   // Clear localStorage for counters before updating the UI
   users.forEach((user) => {
@@ -38,21 +40,22 @@ displayUsers = () => {
       usersList.innerHTML += `<li onClick="handleUserClick(${user.id})">
     <b>ID</b>: ${user.id};
     <b>Name:</b> ${user.name};
-    <span style="display: none;" id="counter${user.id}"></span></li>
+    <b><span style="display: none;" id="counter${user.id}"></span></li></b>
     `;
     }
   });
 };
 
 function handleUserClick(userId) {
-  console.log(`handleUserClick called for user ${userId}`);
+  // console.log(`handleUserClick called for user ${userId}`);
+  searchIcon.style.display = "inline-block";
 
   // Increase the counter for the clicked element by 1
   const clickedCounterElement = document.getElementById(`counter${userId}`);
   if (clickedCounterElement) {
     let count = parseInt(clickedCounterElement.dataset.count) || 0;
     count++;
-    clickedCounterElement.textContent = `counter: ${count}`;
+    clickedCounterElement.textContent = count;
     clickedCounterElement.dataset.count = count;
     clickedCounterElement.style.display = "inline";
   }
@@ -66,7 +69,7 @@ function handleUserClick(userId) {
     if (currentUserId !== "" && currentUserId != userId) {
       let count = parseInt(counterElement.dataset.count) || 0;
       count--;
-      counterElement.textContent = `counter: ${count}`;
+      counterElement.textContent = `${count}`;
       counterElement.dataset.count = count;
       counterElement.style.display = "inline";
     }
@@ -76,23 +79,28 @@ function handleUserClick(userId) {
 function search() {
   let users = generateUsers();
   const searchValue = document.getElementById("search").value.toLowerCase();
-
-  console.log("SearchValue: ", searchValue);
+  searchIcon.style.display = "none";
+  // console.log("SearchValue: ", searchValue);
   let filteredUsers = users.filter((user) => user.name.includes(searchValue));
-  console.log("filtered: ", filteredUsers);
-
-  if (usersList.innerHTML != "") {
+  // console.log("filtered: ", filteredUsers);
+  if (
+    usersList.innerHTML == "" ||
+    usersList.innerHTML ==
+      `<li><b style="color: red;">Press generate button first.</b></li>`
+  ) {
+    usersList.innerHTML = `<li><b style="color: red;">Press generate button first.</b></li>`;
+  } else if (usersList.innerHTML !== "" && filteredUsers.length > 0) {
     usersList.innerHTML = "";
     filteredUsers.map((user) => {
-      let count = parseInt(localStorage.getItem(`counter${user.id}`)) || 0;
+      // let count = parseInt(localStorage.getItem(`counter${user.id}`)) || 0;
 
       usersList.innerHTML += `<li onClick="handleUserClick(${user.id})">
-        <b>ID</b>: ${user.id};
-        <b>Name:</b> ${user.name};
-        <span id="counter${user.id}"></span>
-      </li>`;
+          <b>ID</b>: ${user.id};
+          <b>Name:</b> ${user.name};
+          <b><span style="display: none" id="counter${user.id}"></span></b>
+        </li>`;
     });
-  } else {
-    usersList.innerHTML = `<li><b style="color: red;">Press generate button first.</b></li>`;
+  } else if (filteredUsers.length === 0) {
+    usersList.innerHTML = `<li><b style="color: red;">No match found.</b></li>`;
   }
 }
